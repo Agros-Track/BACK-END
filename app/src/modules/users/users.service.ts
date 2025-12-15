5e77a7b64f01ea2e61c8987bdb95db913b7ce3d6
Sebastian Pineda <sebastian14027@gmail.com>
2025-12-15 00:39:19 -0500

    refactor: remove update action from roles

--- app/src/modules/users/users.service.ts
+++ app/src/modules/users/users.service.ts
@@ -2,6 +2,7 @@ import {
   Injectable,
   BadRequestException,
   NotFoundException,
+  ForbiddenException,
 } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
