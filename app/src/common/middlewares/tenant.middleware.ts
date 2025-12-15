964b230ab6a19a479e8308c2b0bfeb9102104e7f
Shnaider Jordan <shnaiderjordan04@outlook.com>
2025-12-15 02:08:08 -0500

    just figure out the mistake of the tenant id

--- app/src/common/middlewares/tenant.middleware.ts
+++ app/src/common/middlewares/tenant.middleware.ts
@@ -5,13 +5,16 @@ import { Request, Response, NextFunction } from 'express';
 @Injectable()
 export class TenantMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
-    const tenantId = req.headers["x-tenant-id"];
-    
-    if (!tenantId) {
-      return res.status(400).json({ message: "Tenant ID header is missing" });
+
+    const tenantId = req.headers['x-tenant-id'];
+
+    // Store tenantId if present, but don't require it
+    // Auth logic will validate based on user role
+    if (tenantId) {
+      req['tenantId'] = tenantId;
     }
 
-    req['tenantId'] = tenantId;
     next();
   }
 }
+
